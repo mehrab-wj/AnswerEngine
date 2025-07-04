4ce9d2bb78847ae93113e41f11ebbda22dba47fd
Mehrab HP <white_joker20@yahoo.com>
2025-07-05 03:13:25 +0700

    fix: force ssl and trust porixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,11 +14,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use App\Http\Middleware\ForceSSL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withCommands([
@@ -33,6 +34,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ])
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+
+        $middleware->trustProxies(at: '*')->web(append: [
+            ForceSSL::class,
+            HandleAppearance::class,
+            HandleInertiaRequests::class,
+            AddLinkHeadersForPreloadedAssets::class,
+        ]);
 
         $middleware->web(append: [
             HandleAppearance::class,
